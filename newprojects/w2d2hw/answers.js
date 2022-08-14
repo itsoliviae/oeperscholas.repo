c36b67b8549abb2691b77de6d438a1dc83578204
Olivia Eisenman <oliviaeisenman@gmail.com>
2022-08-14 15:17:09 -0400

    Wild Wild Life answered

--- newprojects/w2d2hw/answers.js
+++ newprojects/w2d2hw/answers.js
@@ -1,19 +1,49 @@
 // Easy Going
 ////////////////////////////////
+/*
 for (let i = 1; i <= 20; i++) {
     console.log(i);
 }
-
+*/
 // Get Even
 ////////////////////////////////
+/*
 for (let i = 0; i <= 200; i+= 2) {
     console.log(i);
 }
+*/
 // Fizz Buzz
 ////////////////////////////////
-
+/*
+for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+        console.log("fizzbuzz");
+    } else if (i % 5 === 0) {
+        console.log("buzz")
+    } else if ( i % 3 === 0) {
+        console.log("fizz")
+    }
+}
+*/
 // Wild Wild Life
 ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee.push('20 years old');
+console.log(plantee);
+
+wolfy.splice(3, 3, 'Gotham City');
+console.log(wolfy);
+
+dart.push("Hawkins");
+console.log(dart);
+
+wolfy.splice(0, 0, "Gameboy");
+console.log(wolfy);
 
 // Yell at the Ninja Turtles
 ////////////////////////////////
