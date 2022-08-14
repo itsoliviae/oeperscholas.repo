257cf8860dfbbc5d233eb1bac89815777882019a
Olivia Eisenman <oliviaeisenman@gmail.com>
2022-08-14 16:17:57 -0400

    Where is Waldo Answered

--- newprojects/w2d2hw/answers.js
+++ newprojects/w2d2hw/answers.js
@@ -49,17 +49,60 @@ console.log(wolfy);
 
 // Yell at the Ninja Turtles
 ////////////////////////////////
-
+/*
 const turtles = ['donatello', 'leonardo', 'raphael', 'michaelangelo']
 const tmnt = turtles.map(turtles => turtles.toUpperCase());
 console.log(tmnt);
-
+*/
 
 // Methods, Revisited
 ////////////////////////////////
+/*
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+// console.log(favMovies.indexOf('Titanic'));
+favMovies.sort();
+console.log(favMovies);
+
+favMovies.pop();
+console.log(favMovies);
+
+favMovies.push('Guardians of the Galaxy');
+console.log(favMovies);
+
+favMovies.reverse();
+console.log(favMovies);
+
+favMovies.shift();
+console.log(favMovies);
+
+favMovies.unshift();
+console.log(favMovies);
+
+console.log(favMovies.indexOf('Django Unchained'));
+
+favMovies.splice(14, 14, 'Avatar');
+console.log(favMovies);
+
+favMovies.slice();
+console.log(favMovies);
+*/
+// a removed item returns 'undefined'
 
 // Where is Waldo
 ////////////////////////////////
+/*
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
+whereIsWaldo.splice("Eggbert"); // I can't figure out how to remove something not in the array
+console.log(whereIsWaldo);
+
+console.log(whereIsWaldo.indexOf('Neff'));
+whereIsWaldo.splice(-1, -1, "No One");
+console.log(whereIsWaldo); 
+
+console.log(whereIsWaldo.indexOf("Waldo"));
+*/
 
 //  Excited Kitten
 ////////////////////////////////
